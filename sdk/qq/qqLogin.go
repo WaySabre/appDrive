44f3ff010fd3798a1e6de2a8d65be2de592a3674
WaySabre <78678420+WaySabre@users.noreply.github.com>
2021-03-06 14:25:22 +0800

    add qq

---
+++ sdk/qq/qqLogin.go
@@ -0,0 +1,69 @@
+package qqLogin
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/json"
+	"net/url"
+
+	"github.com/WaySabre/appDrive/sdk/unils"
+)
+
+const (
+	UserInfoURL = "http://openapi.tencentyun.com/v3/user/get_info"
+)
+
+type (
+	// QQ 用户资料
+	QqUserInfo struct {
+		OpenID    string `json:"openid,omitempty"`    // 授权用户唯一标识
+		NickName  string `json:"nickname,omitempty"`  // 普通用户昵称
+		Gender    string `json:"gender,omitempty"`    // 普通用户性别
+		Province  string `json:"province,omitempty"`  // 普通用户个人资料填写的省份
+		City      string `json:"city,omitempty"`      // 普通用户个人资料填写的城市
+		Country   string `json:"country,omitempty"`   // 国家，如中国为CN
+		Figureurl string `json:"figureurl,omitempty"` // 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空
+		ErrCode   uint   `json:"errcode,omitempty"`
+		ErrMsg    string `json:"errmsg,omitempty"`
+	}
+)
+
+type Config struct {
+	Appid string
+	Key   string
+}
+
+// GetUserInfo 获取用户资料
+func (e *Config) GetUserInfo(openId, token, pf string) (QqUserInfo *QqUserInfo, err error) {
+	params := url.Values{
+		"appid":   []string{e.Appid},
+		"format":  []string{"json"},
+		"openid":  []string{openId},
+		"openkey": []string{token},
+		"pf":      []string{pf},
+	}
+	//获取源串
+	sourceUrl := url.QueryEscape(params.Encode())
+	sig := url.QueryEscape(HmacSHA1(e.Key+"&", "GET&%2Fv3%2Fuser%2Fget_info&"+sourceUrl))
+	params.Add("sig", sig)
+	body, err := utils.NewRequest("GET", UserInfoURL, []byte(params.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, &QqUserInfo)
+	if err != nil {
+		return nil, err
+	}
+	QqUserInfo.OpenID = openId
+	return
+}
+
+func HmacSHA1(keyStr, value string) string {
+	key := []byte(keyStr)
+	mac := hmac.New(sha1.New, key)
+	mac.Write([]byte(value))
+	res := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+
+	return res
+}
